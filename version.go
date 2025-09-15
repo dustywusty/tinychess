92ff324e1b15b6b43c17ec1a69f7711c998f2f42
Dusty <dustyc@me.com>
2025-09-15 00:11:11 -0600

    Remove build date and keep captures in panel

--- version.go
+++ version.go
@@ -4,11 +4,9 @@ import (
 	"os/exec"
 	"runtime/debug"
 	"strings"
-	"time"
 )
 
 var commit = "dev"
-var buildDate = ""
 
 func init() {
 	if info, ok := debug.ReadBuildInfo(); ok {
@@ -21,12 +19,6 @@ func init() {
 						commit = commit[:7]
 					}
 				}
-			case "vcs.time":
-				if buildDate == "" && s.Value != "" {
-					if t, err := time.Parse(time.RFC3339, s.Value); err == nil {
-						buildDate = t.Format("2006-01-02")
-					}
-				}
 			}
 		}
 	}
@@ -34,8 +26,5 @@ func init() {
 		if c, err := exec.Command("git", "rev-parse", "--short", "HEAD").Output(); err == nil {
 			commit = strings.TrimSpace(string(c))
 		}
-	}
-	if buildDate == "" {
-		buildDate = time.Now().Format("2006-01-02")
 	}
 }
